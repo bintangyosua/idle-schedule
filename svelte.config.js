d2c8d924114e23355f8bdcce5fdd7e82879881f5
Bintang <itzbintangyosua@gmail.com>
2025-07-01 21:19:39 +0700

    chore: update dependencies and deploy config
    Updates github actions, node version, and svelte config.

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '200.html',
+			// fallback: '200.html',
+			fallback: undefined,
 			precompress: false,
 			strict: true
 		}),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
